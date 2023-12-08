033536e79dfdd8629c0d725f81ebdb8171b67f63
A3DevUser <a3versioncontrol@gmail.com>
2023-12-08 15:31:05 +0530

    change in userData model

--- src/main/java/com/Backend/VueFrame/Model/UserInfo.java
+++ src/main/java/com/Backend/VueFrame/Model/UserInfo.java
@@ -13,63 +13,101 @@ import javax.persistence.Table;
 public class UserInfo { 
 
 	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY) 
-//	@Column(name="id")
+	@Column(name="USERID")
 	private int id; 
-	@Column(name="name")
+	
+	@Column(name="USERNAME")
 	private String name;
-	@Column(name="email")
+	
+	@Column(name = "FIRSTNAME")
+	private String firstName;
+	
+	@Column(name = "LASTNAME")
+	private String lastName;
+	
+	@Column(name="EMAILID")
 	private String email;
-	@Column(name="password")
+	
+	@Column(name="PASSWORD")
 	private String password;
-	@Column(name="roles")
+	
+	@Column(name="VF_ROLE")
 	private String roles;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getRoles() {
 		return roles;
 	}
+
 	public void setRoles(String roles) {
 		this.roles = roles;
 	}
-	public UserInfo(int id, String name, String email, String password, String roles) {
+
+	public UserInfo(int id, String name, String firstName, String lastName, String email, String password,
+			String roles) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.firstName = firstName;
+		this.lastName = lastName;
 		this.email = email;
 		this.password = password;
 		this.roles = roles;
 	}
+
 	public UserInfo() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
-	@Override
-	public String toString() {
-		return "UserInfo [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", roles="
-				+ roles + "]";
-	} 
+	
+	
 	
 	
 
