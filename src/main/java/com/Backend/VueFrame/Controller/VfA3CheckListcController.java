a0dd4ecfe3297cd7729701f53841e7dd7d58cf15
swapnalig <swapnalig@claptek.com>
2023-12-27 13:23:07 +0530

    create new api a3 test data

---
+++ src/main/java/com/Backend/VueFrame/Controller/VfA3CheckListcController.java
@@ -0,0 +1,33 @@
+package com.Backend.VueFrame.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Backend.VueFrame.Model.VfA3CheckListData;
+import com.Backend.VueFrame.Repository.VfA3CheckListRepository;
+
+
+@RestController
+@RequestMapping("VF/")
+@CrossOrigin(origins = "*")
+public class VfA3CheckListcController {
+	
+	
+	@Autowired
+	private VfA3CheckListRepository vfA3CheckListRepo;
+	
+	
+	@GetMapping("VFA3Checklist")
+	public List<VfA3CheckListData> getCheckListData(@RequestParam String formId) {
+		return vfA3CheckListRepo.getByFormId(formId);
+	}
+	
+	
+
+}
