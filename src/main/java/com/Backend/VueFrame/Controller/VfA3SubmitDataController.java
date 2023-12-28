a86d00604ce01a196d599e5603a41b055ed1049f
swapnalig <swapnalig@claptek.com>
2023-12-28 15:49:42 +0530

    add new api for submit data

--- src/main/java/com/Backend/VueFrame/Controller/VfA3SubmitDataController.java
+++ src/main/java/com/Backend/VueFrame/Controller/VfA3SubmitDataController.java
@@ -3,12 +3,13 @@ package com.Backend.VueFrame.Controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.Backend.VueFrame.Model.A3TestData;
 
 @RestController
 @RequestMapping("VF/")
@@ -16,28 +17,40 @@ import org.springframework.web.bind.annotation.RestController;
 public class VfA3SubmitDataController {
 	
 	
-	@PostMapping("SubmitA3Data")
-	public List<String> submitData(@RequestBody List<String> setData) {
+//	@PostMapping("SubmitA3Data")
+//	public List<String> submitData(@RequestBody List<String> setData) {
+//
+//	    List<String> out = new ArrayList<>();
+//
+//	    for (String obj : setData) {
+//	        out.add(obj);
+//	       
+//	        String savedObj = saveAndFlush(obj);
+//	        out.add(savedObj);
+//	    }
+//
+//	    return out;
+//	}
+//
+//	private String saveAndFlush(String obj) {
+//	    
+//	    String savedObj = "Saved: " + obj;
+//	    return savedObj;
+//	}
 
+	
+	@PostMapping("SubmitA3Data")
+	public List<String> submitData(@RequestBody List<A3TestData> testItems) {
 	    List<String> out = new ArrayList<>();
 
-	    for (String obj : setData) {
-	        out.add(obj);
-	       
-	        String savedObj = saveAndFlush(obj);
-	        out.add(savedObj);
+	    for (A3TestData item : testItems) {
+	        // Your processing logic here
+	        out.add(item.getResponse());
 	    }
 
 	    return out;
 	}
 
-	private String saveAndFlush(String obj) {
-	    
-	    String savedObj = "Saved: " + obj;
-	    return savedObj;
-	}
-
-		 
 
 
 	}
---
+++ src/main/java/com/Backend/VueFrame/Model/A3TestData.java
@@ -0,0 +1,215 @@
+package com.Backend.VueFrame.Model;
+
+public class A3TestData {
+	
+	    private String id;
+	    private String objective;
+	    private String process;
+	    private String subProcess;
+	    private String testRef;
+	    private String testTitle;
+	    private String schemeCode;
+	    private String auditId;
+	    private String risk;
+	    private String control;
+	    private String areaName;
+	    private String frequency;
+	    private String dropDown;
+	    private String mixVal;
+	    private String maxVal;
+	    private String formId;
+	    private String resType;
+	    private String response;
+	    private String cost_code_name;
+	    private String vendor_name;
+	    private String act_name;
+	    private String VF_OBJ_ID;
+	    private String VF_STATUS;
+	    private String VF_ACTION;
+		public String getId() {
+			return id;
+		}
+		public void setId(String id) {
+			this.id = id;
+		}
+		public String getObjective() {
+			return objective;
+		}
+		public void setObjective(String objective) {
+			this.objective = objective;
+		}
+		public String getProcess() {
+			return process;
+		}
+		public void setProcess(String process) {
+			this.process = process;
+		}
+		public String getSubProcess() {
+			return subProcess;
+		}
+		public void setSubProcess(String subProcess) {
+			this.subProcess = subProcess;
+		}
+		public String getTestRef() {
+			return testRef;
+		}
+		public void setTestRef(String testRef) {
+			this.testRef = testRef;
+		}
+		public String getTestTitle() {
+			return testTitle;
+		}
+		public void setTestTitle(String testTitle) {
+			this.testTitle = testTitle;
+		}
+		public String getSchemeCode() {
+			return schemeCode;
+		}
+		public void setSchemeCode(String schemeCode) {
+			this.schemeCode = schemeCode;
+		}
+		public String getAuditId() {
+			return auditId;
+		}
+		public void setAuditId(String auditId) {
+			this.auditId = auditId;
+		}
+		public String getRisk() {
+			return risk;
+		}
+		public void setRisk(String risk) {
+			this.risk = risk;
+		}
+		public String getControl() {
+			return control;
+		}
+		public void setControl(String control) {
+			this.control = control;
+		}
+		public String getAreaName() {
+			return areaName;
+		}
+		public void setAreaName(String areaName) {
+			this.areaName = areaName;
+		}
+		public String getFrequency() {
+			return frequency;
+		}
+		public void setFrequency(String frequency) {
+			this.frequency = frequency;
+		}
+		public String getDropDown() {
+			return dropDown;
+		}
+		public void setDropDown(String dropDown) {
+			this.dropDown = dropDown;
+		}
+		public String getMixVal() {
+			return mixVal;
+		}
+		public void setMixVal(String mixVal) {
+			this.mixVal = mixVal;
+		}
+		public String getMaxVal() {
+			return maxVal;
+		}
+		public void setMaxVal(String maxVal) {
+			this.maxVal = maxVal;
+		}
+		public String getFormId() {
+			return formId;
+		}
+		public void setFormId(String formId) {
+			this.formId = formId;
+		}
+		public String getResType() {
+			return resType;
+		}
+		public void setResType(String resType) {
+			this.resType = resType;
+		}
+		public String getResponse() {
+			return response;
+		}
+		public void setResponse(String response) {
+			this.response = response;
+		}
+		public String getCost_code_name() {
+			return cost_code_name;
+		}
+		public void setCost_code_name(String cost_code_name) {
+			this.cost_code_name = cost_code_name;
+		}
+		public String getVendor_name() {
+			return vendor_name;
+		}
+		public void setVendor_name(String vendor_name) {
+			this.vendor_name = vendor_name;
+		}
+		public String getAct_name() {
+			return act_name;
+		}
+		public void setAct_name(String act_name) {
+			this.act_name = act_name;
+		}
+		public String getVF_OBJ_ID() {
+			return VF_OBJ_ID;
+		}
+		public void setVF_OBJ_ID(String vF_OBJ_ID) {
+			VF_OBJ_ID = vF_OBJ_ID;
+		}
+		public String getVF_STATUS() {
+			return VF_STATUS;
+		}
+		public void setVF_STATUS(String vF_STATUS) {
+			VF_STATUS = vF_STATUS;
+		}
+		public String getVF_ACTION() {
+			return VF_ACTION;
+		}
+		public void setVF_ACTION(String vF_ACTION) {
+			VF_ACTION = vF_ACTION;
+		}
+		public A3TestData(String id, String objective, String process, String subProcess, String testRef,
+				String testTitle, String schemeCode, String auditId, String risk, String control, String areaName,
+				String frequency, String dropDown, String mixVal, String maxVal, String formId, String resType,
+				String response, String cost_code_name, String vendor_name, String act_name, String vF_OBJ_ID,
+				String vF_STATUS, String vF_ACTION) {
+			super();
+			this.id = id;
+			this.objective = objective;
+			this.process = process;
+			this.subProcess = subProcess;
+			this.testRef = testRef;
+			this.testTitle = testTitle;
+			this.schemeCode = schemeCode;
+			this.auditId = auditId;
+			this.risk = risk;
+			this.control = control;
+			this.areaName = areaName;
+			this.frequency = frequency;
+			this.dropDown = dropDown;
+			this.mixVal = mixVal;
+			this.maxVal = maxVal;
+			this.formId = formId;
+			this.resType = resType;
+			this.response = response;
+			this.cost_code_name = cost_code_name;
+			this.vendor_name = vendor_name;
+			this.act_name = act_name;
+			this.VF_OBJ_ID = vF_OBJ_ID;
+			this.VF_STATUS = vF_STATUS;
+			this.VF_ACTION = vF_ACTION;
+		}
+		public A3TestData() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+		
+		
+	   
+	    
+	    
+
+
+}
