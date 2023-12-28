f5572ee84e92597008eba91380015efe77e233ff
swapnalig <swapnalig@claptek.com>
2023-12-28 18:18:53 +0530

    create api for onboarding checklist submit

--- src/main/java/com/Backend/VueFrame/Controller/VfA3SubmitDataController.java
+++ src/main/java/com/Backend/VueFrame/Controller/VfA3SubmitDataController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,6 +42,11 @@ public class VfA3SubmitDataController {
 	
 	
 	
+	@GetMapping("fetchDataId")
+	public List<A3TestData> getData() {
+		
+		return vfa3TestRepo.findAll();
+	}
 
 
 
