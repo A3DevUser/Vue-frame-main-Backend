04977dace9d7bc80d15aa0a66c49736475e8bf11
swapnalig <swapnalig@claptek.com>
2024-01-24 13:37:07 +0530

    new api for post review plan data

--- src/main/java/com/Backend/VueFrame/Controller/A3ReviewPlanController.java
+++ src/main/java/com/Backend/VueFrame/Controller/A3ReviewPlanController.java
@@ -15,6 +15,9 @@ import com.Backend.VueFrame.Model.VfA3ReviewPlan;
 import com.Backend.VueFrame.Model.VfA3ReviewPlanStatus;
 import com.Backend.VueFrame.Repository.A3ReviewPlanRepo;
 import com.Backend.VueFrame.Repository.A3ReviewPlanStatusRepo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 @RestController
 @RequestMapping("VF/")
@@ -57,6 +60,47 @@ public class A3ReviewPlanController {
 	    a3ReviewPlanStatusRepo.saveAndFlush(reviewPlanStatusList.get(0)); 
 
 	    return "Reviews saved successfully!";
+	}
+	
+	
+	
+//	@PostMapping("updateStatusToAccept")
+//	public void setStatus(@RequestBody List<ReviewData> reviewDataList) {
+//		
+//		List<VfA3ReviewPlanStatus> reviewPlanStatusList = new ArrayList<>();
+//		
+//		for(ReviewData reviewData :  reviewDataList) {
+//			
+//			
+//			 VfA3ReviewPlanStatus reviewPlanStatus = new VfA3ReviewPlanStatus(reviewData.getReviewId(),
+//		                reviewData.getReviewName(), reviewData.getReviewStatus(), reviewData.getReviewComment());
+//		        reviewPlanStatusList.add(reviewPlanStatus);
+//		        
+//		        
+//		
+//		        return;
+//		        
+//	}
+//	}
+	
+	
+	
+
+	@PostMapping("updateStatusToAccept")
+	public void setStatus(@RequestBody List<ReviewData> reviewDataList) {
+	    for (ReviewData reviewData : reviewDataList) {
+	        if ("accepted".equalsIgnoreCase(reviewData.getReviewStatus())) {
+	            try {
+	                ObjectMapper objectMapper = new ObjectMapper();
+	                String jsonData = objectMapper.writeValueAsString(reviewData);
+
+	                a3ReviewPlanStatusRepo.setReviewPlan(jsonData);
+	            } catch (Exception e) {
+	                // Handle the exception or log an error
+	                e.printStackTrace();
+	            }
+	        }
+	    }
 	}
 
 }
--- src/main/java/com/Backend/VueFrame/Model/ReviewData.java
+++ src/main/java/com/Backend/VueFrame/Model/ReviewData.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ReviewData {
 
-	
+	private String reviewId;
 	private String reviewName;
 	@JsonProperty(value = "ASSOCIATE_VEND")
     private String ASSOCIATE_VEND;
@@ -12,8 +12,20 @@ public class ReviewData {
     private String VENDOR_ID;
     private String reviewStatus;
     private String reviewComment;
+    private String Review_Type;
+    private String Review_Freq;
+    private String Sub_Frequency;
     
     
+    
+    
+    
+	public String getReviewId() {
+		return reviewId;
+	}
+	public void setReviewId(String reviewId) {
+		this.reviewId = reviewId;
+	}
 	public String getReviewName() {
 		return reviewName;
 	}
@@ -45,15 +57,49 @@ public class ReviewData {
 	public void setReviewComment(String reviewComment) {
 		this.reviewComment = reviewComment;
 	}
-	public ReviewData(String reviewName, String ASSOCIATE_VEND, String VENDOR_ID, String reviewStatus,
-			String reviewComment) {
+	
+	
+	
+	public String getReview_Type() {
+		return Review_Type;
+	}
+	public void setReview_Type(String Review_Type) {
+		Review_Type = Review_Type;
+	}
+	public String getReview_Freq() {
+		return Review_Freq;
+	}
+	public void setReview_Freq(String Review_Freq) {
+		Review_Freq = Review_Freq;
+	}
+	public String getSub_Frequency() {
+		return Sub_Frequency;
+	}
+	public void setSub_Frequency(String Sub_Frequency) {
+		Sub_Frequency = Sub_Frequency;
+	}
+	
+	
+	
+	public ReviewData(String reviewId, String reviewName, String ASSOCIATE_VEND, String VENDOR_ID, String reviewStatus,
+			String reviewComment, String Review_Type, String Review_Freq, String Sub_Frequency) {
 		super();
+		this.reviewId = reviewId;
 		this.reviewName = reviewName;
 		this.ASSOCIATE_VEND = ASSOCIATE_VEND;
 		this.VENDOR_ID = VENDOR_ID;
 		this.reviewStatus = reviewStatus;
 		this.reviewComment = reviewComment;
+		this.Review_Type = Review_Type;
+		this.Review_Freq = Review_Freq;
+		this.Sub_Frequency = Sub_Frequency;
 	}
+	public ReviewData() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 	
 	
     
